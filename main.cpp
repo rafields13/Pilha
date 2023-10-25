85635dc32c0cd5a367e8e835e7cb3d7b56a820f9
Rafael <rafaelmarliere033@gmail.com>
2023-10-25 11:09:08 -0300

    ajustes necessarios feitos

--- main.cpp
+++ main.cpp
@@ -1,40 +1,30 @@
 #include <iostream>
-#include <conio.h>
 
 struct Pilha {
-    int dado;
+    char dado;
     struct Pilha *prox;
 };
 
 typedef struct Pilha pilha;
 typedef pilha *pilhaPtr;
 
-void menu() {
-    std::cout << "Escolha a opcao:\n";
-    std::cout << "1 - PUSH: Empilhar um ou mais valores na Pilha\n";
-    std::cout << "2 - POP: Retirar o ultimo valor da Pilha\n";
-    std::cout << "3 - REMOVE: Retira qualquer valor da pilha\n";
-    std::cout << "4 - IMPRIME: Imprime a pilha\n";
-    std::cout << "5 - Para fechar o programa\n";
-}
-
-void push(pilhaPtr *topo, int info) {
+void push(pilhaPtr *topo, char info) {
     auto novo = (pilhaPtr) malloc(sizeof(pilha));
 
     if (novo != nullptr) {
         novo->dado = info;
         novo->prox = *topo;
         *topo = novo;
-    } else {
-        std::cout << info << " nao inserido. Sem memoria disponivel.\n";
-    }
+    } else
+        std::cout << "Elemento \"" << info << "\" nao inserido. Sem memoria disponivel.\n";
 }
 
-int pop(pilhaPtr *topo) {
+char pop(pilhaPtr *topo) {
     pilhaPtr aux = *topo;
-    int valor = (*topo)->dado;
+    char valor = (*topo)->dado;
     *topo = (*topo)->prox;
-    delete(aux);
+    free(aux);
+
     return valor;
 }
 
@@ -42,100 +32,33 @@ int vazia(pilhaPtr topo) {
     return topo == nullptr;
 }
 
-void remove(pilhaPtr *topo, int valor) {
-    pilhaPtr pilhaAux = nullptr, pilha = *topo;
-    int i = 0;
+bool verificaExpressao(const std::string &expressao) {
+    pilhaPtr P = nullptr;
 
-    while (i != 1 && pilha != nullptr) {
-        if (pilha->dado == valor)
-            i = 1;
-        else
-            pilha = pilha->prox;
-    }
+    for (char caractere: expressao) {
+        if (caractere == '(' || caractere == '[' || caractere == '{')
+            push(&P, caractere);
+        else if (caractere == ')' || caractere == ']' || caractere == '}') {
+            if (vazia(P))
+                return false;
 
-    if (i == 1) {
-        while ((*topo)->dado != valor)
-            push(&pilhaAux, pop(topo));
+            char topo = pop(&P);
 
-        pop(topo);
-
-        while (pilhaAux != nullptr)
-            push(topo, pop(&pilhaAux));
-
-        std::cout << "\nValor \" " << valor << "\" removido da pilha.\n";
-    } else
-        std::cout << "Valor \\" << valor << "\" nao encontrado na pilha!\n";
-}
-
-void imprimePilha(pilhaPtr aux) {
-    std::cout << "\n";
-
-    if (aux == nullptr) {
-        std::cout << "A pilha esta vazia!\n";
-        std::cout << "\nPressione enter para voltar ao menu.";
-        getch();
-    } else {
-        std::cout << "Pilha:";
-
-        while (aux != nullptr) {
-            std::cout << " " << aux->dado << " ";
-            aux = aux->prox;
+            if ((caractere == ')' && topo != '(') || (caractere == ']' && topo != '[') ||
+                (caractere == '}' && topo != '{'))
+                return false;
         }
-
-        std::cout << "\nPressione enter para voltar ao menu.";
-        getch();
     }
+
+    return vazia(P);
 }
 
 int main() {
-    pilhaPtr P = nullptr;
-    int opcao, quantidadeDados, i, valor;
+    std::string expressao;
+    std::cout << "Digite a expressao a ser verificada:";
+    getline(std::cin, expressao);
 
-    menu();
-    std::cin >> opcao;
+    verificaExpressao(expressao) ? std::cout << "A expressao e valida.\n" : std::cout << "A expressao e invalida.\n";
 
-    do {
-        switch (opcao) {
-            case 1:
-                std::cout << "Quantidade de dados a inserir:";
-                std::cin >> quantidadeDados;
-
-                for (i = 0; i < quantidadeDados; i++) {
-                    std::cout << "\nEscreva o valor do " << i + 1 << ":";
-                    std::cin >> valor;
-                    push(&P, valor);
-                }
-
-                imprimePilha(P);
-                break;
-            case 2:
-                if (!vazia(P))
-                    std::cout << "Valor desempilhado \"" << pop(&P) << "\".\n";
-
-                imprimePilha(P);
-                break;
-            case 3:
-                std::cout << "Valor a ser removido:\n";
-                std::cin >> valor;
-                remove(&P, valor);
-                imprimePilha(P);
-                break;
-            case 4:
-                imprimePilha(P);
-                break;
-            case 5:
-                delete(P);
-                break;
-            default:
-                std::cout << "Opcao invalida!\n";
-                menu();
-                break;
-        }
-
-        if (opcao != 5) {
-            std::cout << "\n";
-            menu();
-            std::cin >> opcao;
-        }
-    } while (opcao != 5);
+    return 0;
 }
